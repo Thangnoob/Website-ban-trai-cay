02d2e2f53756711cbf1c4a0ef82085c1bc814542
tai-smile25 <huutaia33@gmail.com>
2025-05-22 15:44:07 +0700

    fix load images

---
+++ backend/product-service/src/main/java/vn/iuh/fit/se/productservice/config/WebConfig.java
@@ -0,0 +1,17 @@
+package vn.iuh.fit.se.productservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Cho phép tất cả các đường dẫn
+                .allowedOrigins("http://localhost:3000") // FE chạy ở cổng 3000 (React, Vue, v.v.)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Cho phép các phương thức này
+                .allowedHeaders("*") // Cho phép tất cả headers
+                .allowCredentials(true); // Cho phép gửi cookie nếu cần
+    }
+}
